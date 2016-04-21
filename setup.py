317997642786d114f07cb61682d606af3a5505fc
Tarun Mukherjee <tmukherjee13@gmail.com>
2016-04-22 01:15:11 +0530

    created setup script

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="Passman",
+
+    # Version number (initial):
+    version="0.1.0",
+
+    # Application author details:
+    author="Tarun Mukherjee",
+    author_email="tmukherjee13@gmail.com",
+
+    # Packages
+    packages=["app","cli"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="https://github.com/tmukherjee13/passman",
+
+    #
+    # license="LICENSE.txt",
+    description="A Simple Password Manager",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "django",
+        "pyperclip",
+        "argparse",
+        "wsgiref"
+    ],
+)
